416e184db0b50c7481390d7050f14a600b5f759c
william <williamrutynamacedo@gmail.com>
2025-10-06 10:04:15 -0300

    deploy com o dominio funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/macedog/">
+      <BrowserRouter >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/macedog/",
+  base: "/",
   plugins: [
     react(),
     mode === 'development' &&
